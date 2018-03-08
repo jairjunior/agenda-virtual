16846e9250e61fb40f88c3e208cfeb72c92f608a
Jair Junior <contato@jairjunior.eng.br>
2018-03-08 19:53:18 -0300

    Feito teste para mostrar a imagem do perfil do usuário através da variável de sessão. Funcionando.

--- _view/agenda.php
+++ _view/agenda.php
@@ -1,19 +1,65 @@
 <?php
 	session_start();
 ?>
-
-<!DOCTYPE html>
-<html lang="pt-br">
 <head>
-	<title>Agenda de Contatos</title>
-</head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Agenda virtual para manter contatos">
+    <meta name="author" content="Jair Junior">
+    <meta name="keywords" content="HTML,CSS,PHP,JavaScript,jQuery,MySQL,Agenda Virtual,Contatos,Aplicação Web">
 
+	<title>Agenda Virtual</title>
+
+	<!-- Bootstrap 4.0.0 CSS (compiled and minified) -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- Font Awesome -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
 <body>
-<?php
+<!--********************************************************************-->
+<!--************************  PERFIL DO USUÁRIO  ***********************-->
+<!--********************************************************************-->
+<div class="container-fluid my-top-container">
+	
+	<?php 
+		var_dump( $_SESSION["loggedInUserPhoto"] );
+	?>
+
+
+	<img src="<?php echo($_SESSION["loggedInUserPhoto"]) ?>" alt="..." class="img-thumbnail">
+
+
+	<?php
 	echo "<h1>Seja bem-vindo ".$_SESSION["loggedInUserName"]." ".$_SESSION["loggedInUserLastName"]."</h1>";
-?>
+	?>
+</div>
+
+
+
+
+	<!-- jQuery 3.2.1 library -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+
+    <!-- Popper 1.12.9 library -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
+    <!-- Bootstrap JavaScript plugin (compiled and minified) -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 </body>
 </html>
+
+
 
 
 
