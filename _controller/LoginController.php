7938d89c53cb22a23c27d5d30e1f0d2cbb2737c2
Jair Junior <contato@jairjunior.eng.br>
2018-03-08 19:51:40 -0300

    Criada duas variáveis de seção: uma para a pasta de imagens do usuário e outra para a foto do perfil do usuário.

--- _controller/LoginController.php
+++ _controller/LoginController.php
@@ -2,6 +2,8 @@
 	session_start();
 	require_once("../_model/ConnectToDatabase.php");
 	require_once("../_model/UserDAO.php");
+	require_once("DirManagement.php");
+	require_once("PhotoManagement.php");
 
 
 	if( isset($_POST["email"]) && isset($_POST["password"]) ){
@@ -20,6 +22,23 @@
 				$_SESSION["loggedInUserLastName"] = $user->getLastName();
 				$_SESSION["loggedInUserEmail"] = $user->getEmail();
 				$_SESSION["loggedInUserPermission"] = $user->getAdmin();
+				$_SESSION["loggedInUserImgDir"] = "../_images/".$user->getIdUser()." - ".$user->getName();
+
+				// Estando logado, verifica a existência da pasta de imagens do usuário
+				// Se não existir, ela será criada.
+				$dirMngmt = new DirManagement();
+				if( !$dirMngmt->isUserImgDir( $_SESSION["loggedInUserImgDir"] ) )
+					$dirMngmt->createUserImgDir( $_SESSION["loggedInUserImgDir"] );
+
+				// Também verifica se já existe a foto de perfil do usuário.
+				// Se não existir, será usada a imagem padrão de acordo com o sexo.
+				$photoMngmt = new PhotoManagement();
+				$imgPath = $_SESSION["loggedInUserImgDir"]."/".$user->getIdUser()."-".$user->getName()."-profile.jpg";
+				if( $photoMngmt->isUserPhoto($imgPath) )
+					$_SESSION["loggedInUserPhoto"] = $imgPath;
+				else
+					$_SESSION["loggedInUserPhoto"] = "../_images/general_male_user_profile_image.jpg";
+
 
 				header('Location: ../_view/agenda.php');
 			}
